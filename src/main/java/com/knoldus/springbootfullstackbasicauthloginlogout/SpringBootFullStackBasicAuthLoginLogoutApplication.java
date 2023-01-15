c083ca67581bffb9b8eb12096d04dc6922394bba
Shashikant-15 <shashikant.tanti@knoldus.com>
2023-01-15 19:00:08 +0530

    final commit

---
+++ src/main/java/com/knoldus/springbootfullstackbasicauthloginlogout/SpringBootFullStackBasicAuthLoginLogoutApplication.java
@@ -0,0 +1,13 @@
+package com.knoldus.springbootfullstackbasicauthloginlogout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullStackBasicAuthLoginLogoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFullStackBasicAuthLoginLogoutApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/knoldus/springbootfullstackbasicauthloginlogout/course/Course.java
@@ -0,0 +1,7 @@
+package com.knoldus.springbootfullstackbasicauthloginlogout.course;
+
+public record Course(Long id,
+                     String username,
+                     String description) {
+
+}
